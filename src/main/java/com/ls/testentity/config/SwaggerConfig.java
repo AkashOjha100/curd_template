d57732e12b1565adecb7070b569a2aa4c776494b
AkashOjha100 <akash2002vip@gmail.com>
2026-02-17 18:10:22 +0530

    new Update

--- src/main/java/com/ls/testentity/config/SwaggerConfig.java
+++ src/main/java/com/ls/testentity/config/SwaggerConfig.java
@@ -1,29 +1,45 @@
 package com.ls.testentity.config;
 
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.Components;
-import io.swagger.v3.oas.models.security.SecurityScheme;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@SecurityScheme(
+        name = "BearerAuth",
+        type = SecuritySchemeType.HTTP,
+        scheme = "bearer",
+        bearerFormat = "JWT",
+        in = SecuritySchemeIn.HEADER
+)
 public class SwaggerConfig {
 
     @Bean
-    public OpenAPI openAPI() {
+    public OpenAPI openAPI(
+            @Value("TestAllCurd with JWT") String appTitle,
+            @Value("1.0.2602") String appVersion
+    ) {
 
         return new OpenAPI()
+                .info(new io.swagger.v3.oas.models.info.Info()
+                        .title(appTitle)
+                        .version(appVersion))
                 .addSecurityItem(new SecurityRequirement().addList("BearerAuth"))
                 .components(new Components()
                         .addSecuritySchemes("BearerAuth",
-                                new SecurityScheme()
+                                new io.swagger.v3.oas.models.security.SecurityScheme()
                                         .name("Authorization")
-                                        .type(SecurityScheme.Type.HTTP)
+                                        .type(io.swagger.v3.oas.models.security.SecurityScheme.Type.HTTP)
                                         .scheme("bearer")
                                         .bearerFormat("JWT")));
     }
-
-
 }
